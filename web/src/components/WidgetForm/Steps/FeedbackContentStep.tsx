37eb7c3c535656b96f104ddceacbf4acc36d447c
lepitsch <72175601+lepitsch@users.noreply.github.com>
2022-05-15 11:17:37 -0300

    Comunicação de componentes

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,18 +1,29 @@
+import { ArrowLeft } from 'phosphor-react'
 import { FeedbackType, feedbackTypes } from '..'
 import { CloseButton } from '../../CloseButton'
 
 interface FeedbackContentStepsProps {
   feedbackType: FeedbackType
+  onFeedbackRestartRequest: () => void
 }
 
 export function FeedbackContentStep({
-  feedbackType
+  feedbackType,
+  onFeedbackRestartRequest
 }: FeedbackContentStepsProps) {
   const feedbackTypeInfo = feedbackTypes[feedbackType]
 
   return (
     <>
       <header>
+        <button
+          type="button"
+          className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
+          onClick={onFeedbackRestartRequest}
+        >
+          <ArrowLeft weight="bold" className="w-4 h-4" />
+        </button>
+
         <span className="text-xl leading-6 flex items-center gap-2">
           <img
             src={feedbackTypeInfo.image.source}
