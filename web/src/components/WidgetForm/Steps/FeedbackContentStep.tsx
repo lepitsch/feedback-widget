2b811e2e7923523846e65bbe25938b6b4c1cdfe3
lepitsch <72175601+lepitsch@users.noreply.github.com>
2022-10-01 00:41:25 -0300

    Sucesso do feedback

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -7,11 +7,13 @@ import { ScreenshotButton } from '../ScreenshotButton'
 interface FeedbackContentStepsProps {
   feedbackType: FeedbackType
   onFeedbackRestartRequested: () => void
+  onFeedbackSent: () => void
 }
 
 export function FeedbackContentStep({
   feedbackType,
-  onFeedbackRestartRequested
+  onFeedbackRestartRequested,
+  onFeedbackSent
 }: FeedbackContentStepsProps) {
   const [screenshot, setScreenshot] = useState<string | null>(null)
   const [comment, setComment] = useState('')
@@ -25,6 +27,8 @@ export function FeedbackContentStep({
       screenshot,
       comment
     })
+
+    onFeedbackSent()
   }
 
   return (
